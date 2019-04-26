f21300121b06b5c7fda542a5b3c075c1d71894ff
Jascha <jascha@jaeh.at>
2019-04-26 02:04:26 +0200

    assets: split Hat from WNWLogo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   LanguageSwitch: require('@magic-modules/languageswitch'),
 
+  Hat: require('./Hat'),
   WNWLogo: require('./WNWLogo'),
   Hex: require('./Hex'),
   KickStarter: require('./KickStarter'),
